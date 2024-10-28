3447bd46d39dd11cfca1c70459f36cde3e6ba53d
Abraham <penaabraham@micorreo.upp.edu.mx>
2024-10-28 01:25:33 -0600

    Initial commit

--- resources/js/Pages/Pizzas/Edit.jsx
+++ resources/js/Pages/Pizzas/Edit.jsx
@@ -1,14 +1,18 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 import UpdatePizzaOrderForm from "@/Pages/Pizzas/Partials/UpdatePizzaOrderForm.jsx";
 
 export default function Edit({ auth, pizza }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Order #{pizza.id}</h2>}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Order #{pizza.id}
+                </h2>
+            }
         >
-            <Head title={'Order #' + pizza.id} />
+            <Head title={"Order #" + pizza.id} />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
